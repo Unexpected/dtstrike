4ca51e0d69a240bf1176f606c18e0e2da5c8c69b
Sébastien SCHMITT <sebastien.schmitt@logica.com>
2013-06-17 09:10:26 +0200

    Ajout de la table des sessions CodeIgniter

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -218,3 +218,14 @@ CREATE TABLE `worker` (
   PRIMARY KEY (`worker_id`),
   UNIQUE KEY `api_key` (`api_key`)
 );
+
+DROP TABLE IF EXISTS `ci_sessions`;
+CREATE TABLE `ci_sessions` (
+session_id VARCHAR( 40 ) DEFAULT  '0' NOT NULL ,
+ip_address VARCHAR( 45 ) DEFAULT  '0' NOT NULL ,
+user_agent VARCHAR( 120 ) NOT NULL ,
+last_activity INT( 10 ) UNSIGNED DEFAULT 0 NOT NULL ,
+user_data TEXT NOT NULL ,
+PRIMARY KEY ( session_id ) ,
+KEY  `last_activity_idx` (  `last_activity` )
+);
