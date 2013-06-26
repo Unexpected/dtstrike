4372c99278dc51b1f4b4e5dac7fe20b86c5a527b
Sébastien SCHMITT <sebastien.schmitt@logica.com>
2013-06-26 08:35:23 +0200

    Suppression d'options inutiles

--- website/api/server_info.php
+++ website/api/server_info.php
@@ -10,8 +10,6 @@ $server_info = array(
     "db_password" 	=> $db[$active_group]['password'],
     "db_name" 		=> $db[$active_group]['database'],
     "mailer_address" => "donotsend",
-    "aws_accesskey" => "",
-    "aws_secretkey" => "",
     "submissions_open" => True,
     "repo_path" => $repo_dir,
     "uploads_path" => $upload_dir,
@@ -19,26 +17,7 @@ $server_info = array(
     "replay_path" => $replay_dir,
     "api_create_key" => $api_create_key,
     "api_log" => $api_log,
-    "game_result_errors" => $game_result_errors,
-    "game_options" => array (
-        "turns" => 1500,
-        "loadtime" => 3000,
-        "turntime" => 500,
-        "viewradius2" => 77,
-        "attackradius2" => 5,
-        "spawnradius2" => 1,
-        "location" => "{api_url}",
-        "serial" => 2,
-        "food_rate" => array(5,11),
-        "food_turn" => array(19,37),
-        "food_start" => array(75,175),
-        "food_visible" => array(3,5),
-        "food" => "symmetric",
-        "attack" => "focus",
-        "kill_points" => 2,
-        "cutoff_turn" => 150,
-        "cutoff_percent" => 0.85
-    )
+    "game_result_errors" => $game_result_errors
 );
 
 ?>
