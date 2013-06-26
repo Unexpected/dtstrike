a646bb42d8f9e39d131591ac210d710415bfd5b9
Sébastien SCHMITT <sebastien.schmitt@logica.com>
2013-06-26 16:46:22 +0200

    Correction nom param

--- starters/java/src/EconomicPlanet.java
+++ starters/java/src/EconomicPlanet.java
@@ -2,9 +2,9 @@ public class EconomicPlanet extends Planet {
 
 	public int revenue;
 
-	public EconomicPlanet(int id, int owner, int numShips, int growthRate,
+	public EconomicPlanet(int id, int owner, int numShips, int revenue,
 			double x, double y) {
 		super(id, owner, numShips, x, y);
-		this.revenue = growthRate;
+		this.revenue = revenue;
 	}
 }
