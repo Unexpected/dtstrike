474b674e7139c8d56a2636706b310d8d75ce7ee8
Sébastien SCHMITT <sebastien.schmitt@logica.com>
2013-06-26 15:21:31 +0200

    Affichage du visilizer

--- website/application/views/game/game_view.php
+++ website/application/views/game/game_view.php
@@ -1,5 +1,35 @@
 
-<p>
-	<h2>Visualisateur de parties</h2>
-Inclure le JS ICI
-</p>
+  <div id="container">
+    <header>
+	  <div id="players">Loading</div>
+    </header>
+    
+    <div id="main">
+        <canvas id="display" width="640" height="640"></canvas>
+        <p id="controls">
+        	| 
+            <a href="#" id="slow-button">-</a> | 
+            <a href="#" id="start-button">&laquo;</a> | 
+            <a href="#" id="prev-frame-button">&laquo;</a> | 
+            <a href="#" id="play-button">&#9654;</a> | 
+            <a href="#" id="next-frame-button">&raquo;</a> | 
+            <a href="#" id="end-button">&raquo;</a> | 
+            <a href="#" id="fast-button">+</a> |
+        </p>
+        <p>
+		  <div style="height: 20px;">
+			  <span style="float: left">Match: <span id="macthId">Loading</span></span>
+			  <span style="float: right">Turn: <span id="turnCounter">Loading</span></span>
+		  </div>
+		  <div style="height: 100px;">
+			  <canvas id="chart" width="640" height="100" style="position: absolute; z-index: 1" ></canvas>
+			  <canvas id="feedline" width="640" height="100" style="position: absolute; background-color: transparent; z-index: 2" ></canvas>
+		  </div>
+        </p>
+    </div>
+  </div> <!-- end of #container -->
+
+  <link rel="stylesheet" href="<?php echo base_url("visualizer/inc/style.css?v=1") ?>">
+  <script type="text/javascript" src="<?php echo base_url("visualizer/game.js") ?>"></script>
+  <?php /*echo $replay_data;*/ ?>
+  <script type="text/javascript" src="<?php echo base_url("visualizer/inc/visualizer.js?v=1") ?>"></script>
