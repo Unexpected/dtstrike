b37a7de78b23e2559e8425b1f21bd87f8df2d9b2
Sébastien SCHMITT <sebastien.schmitt@logica.com>
2013-07-12 17:37:33 +0200

    Modification message d'accueil

--- website/application/views/welcome_message.php
+++ website/application/views/welcome_message.php
@@ -4,22 +4,23 @@
 <p>Ce projet vous est présenté par la DT Six.<br/>
 C'est ce qu'on appelle un concours d'Intelligence Artificielle (AI en Anglais).<br/>
 <br/>
-Les plus connus de ces concours sont ceux organisés par Google et nommés <code>Google AI Challenge</code>.<br/>
+Les plus connus de ces concours sont ceux organisés par Google et l'Université de Waterloo et nommés <code>Google AI Challenge</code>.<br/>
 Il y a eux diverses versions au fil des années :
+</p>
 <ul>
 	<li>Tron (Winter 2009)</li>
 	<li><a href="http://planetwars.aichallenge.org">Planet Wars (Fall 2010)</a></li>
 	<li><a href="http://aichallenge.org">Ants (Fall 2011)</a></li>
 </ul><br/>
-</p>
 
 <p>
 L'objectif de ce concour est à la fois très simple et assez challengeant !<br/>
-Vous devez écrire une intelligence artificielle plus aboutie que celle des autres membres <code>CGI</code>.<br/>
+Vous devez écrire une intelligence artificielle plus aboutie que celle des autres <code>membres CGI</code>.<br/>
 <br/>
 Pour cela, vous avez à votre disposition des Starter Kits dans différents languages.<br/>
 Ou même pour débuter, vous avez accès à une version totalement en ligne (à venir).
 </p>
-
+<br/>
+<br/>
 <code>Stay Tuned</code>
-<p style="text-align: right;"><br/><br/>May the DT be with you.</p>
+<p style="text-align: right;"><br/>May the DT be with you.</p>
