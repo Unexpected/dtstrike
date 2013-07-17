efb4710ccbbb79bcef076f3b4b7455d005e061de
Sébastien SCHMITT <sebastien.schmitt@logica.com>
2013-07-17 16:10:48 +0200

    Amélioration build et script de lancement

--- server/ITWarsEngine/scripts/run.bat
+++ server/ITWarsEngine/scripts/run.bat
@@ -2,8 +2,8 @@
 cls
 
 if "x%1" == "x" goto usage
-if "x%2" == "x" goto usage
-if "x%3" == "x" goto usage
+if x%2 == x goto usage
+if x%3 == x goto usage
 set map=%1
 set bot1=%2
 set bot2=%3
@@ -12,8 +12,8 @@ echo Running game beetwen '%bot1%' and '%bot2%' on '%map%'
 echo.
 
 :runEngine
-java -jar engine.jar Engine %map% 1000 1000 log.txt %bot1% %bot2%
-if not "%errorlvel%" == 0 goto error
+java -jar engine.jar %map% 1000 1000 log.txt %bot1% %bot2% 1>game.txt 2>game_err.txt
+if not "%errorlevel%" == "0" goto error
 echo Game ended
 goto end
 
@@ -23,7 +23,7 @@ goto end
 
 :usage
 echo Wrong usage
-echo Usage : run.bat "MapName" "MyFirstBot" "MySecondBot"
+echo Usage : run.bat MapName "command for MyFirstBot" "command for MySecondBot"
 
 :end
-REM pause
+pause
