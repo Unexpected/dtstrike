f4d5096bff90e90d623e789710a367541f23d46e
Sébastien SCHMITT <sebastien.schmitt@logica.com>
2013-08-20 14:21:42 +0200

    Ajout de données d'init pour tester (4 users avec 1 soumission chacun)

--- sql/03_test_data.sql
+++ sql/03_test_data.sql
@@ -1,18 +1,25 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
--- Seb
-INSERT INTO `user` (`user_id`, `username`, `email`, `org_id`, `bio`, `country_code`, `created`, `shutdown_date`, `max_game_id`) VALUES
-(1, 'schmittse', 'sebastien.schmitt@cgi.com', 1, NULL, 'FR', '2013-06-26 08:17:26', NULL, NULL);
+-- Create players and roles
+INSERT INTO `user` (`user_id`, `username`, `email`, `org_id`, `country_code`, `created`, `shutdown_date`) 
+VALUES
+	(1, 'schmittse', 'sebastien.schmitt@cgi.com', 1, 'FR', current_timestamp(), timestampadd(hour, 72, current_timestamp)),
+	(2, 'bellangerf', 'francis.bellanger@cgi.com', 1, 'FR', current_timestamp(), timestampadd(hour, 72, current_timestamp)),
+	(3, 'test1', 'test1@cgi.com', 1, 'FR', current_timestamp(), timestampadd(hour, 72, current_timestamp)),
+	(4, 'test2', 'test2@cgi.com', 1, 'FR', current_timestamp(), timestampadd(hour, 72, current_timestamp));
 INSERT INTO `user_roles` (`user_id`, `role_name`) VALUES
-(1, 'ADMIN');
-
--- Francis
-INSERT INTO `user` (`user_id`, `username`, `email`, `org_id`, `bio`, `country_code`, `created`, `shutdown_date`, `max_game_id`) VALUES
-(2, 'bellangerf', 'francis.bellanger@cgi.com', 1, NULL, 'FR', '2013-06-26 08:17:26', NULL, NULL);
-INSERT INTO `user_roles` (`user_id`, `role_name`) VALUES
-(2, 'ADMIN');
-
+	(1, 'ADMIN'),
+	(2, 'ADMIN'),
+	(3, 'USER'),
+	(4, 'USER');
 
 INSERT INTO `map` (`map_id`, `filename`, `priority`, `players`, `max_turns`, `timestamp`) VALUES
-(1, 'map1.txt', 1, 2, 1000, '2013-06-26 08:26:55'),
-(2, 'map2.txt', 1, 4, 1000, '2013-06-26 08:26:55');
+	(1, 'map1.txt', 1, 2, 1000, current_timestamp()),
+	(2, 'map2.txt', 1, 4, 1000, current_timestamp());
+
+-- Create 1 submission for each player
+INSERT INTO `submission` (`submission_id`, `user_id`, `version`, `status`, `timestamp`, `language_id`) VALUES
+	(1, 1, 1, 20, current_timestamp(), 0),
+	(2, 2, 1, 20, current_timestamp(), 0),
+	(3, 3, 1, 20, current_timestamp(), 0),
+	(4, 4, 1, 20, current_timestamp(), 0);
