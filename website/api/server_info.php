4efb021737ed88b119f57c57bdca762ab87c5feb
Sébastien SCHMITT <sebastien.schmitt@logica.com>
2013-10-31 09:12:13 +0100

    Fix configs

--- website/api/server_info.php
+++ website/api/server_info.php
@@ -9,8 +9,8 @@ $server_info = array(
     "db_username" 	=> $db[$active_group]['username'],
     "db_password" 	=> $db[$active_group]['password'],
     "db_name" 		=> $db[$active_group]['database'],
-    "mailer_address" => "donotsend",
-    "submissions_open" => True,
+    "mailer_address" => $config['mailer_address'],
+    "submissions_open" => $config['submissions_open'],
     "repo_path" => $config['repo_dir'],
     "uploads_path" => $config['upload_dir'],
     "maps_path" => $config['map_dir'],
@@ -18,11 +18,11 @@ $server_info = array(
     "api_create_key" => $config['api_create_key'],
     "api_log" => $config['api_log'],
     "game_result_errors" => $config['game_result_errors'],
-	"game_options" => array(
+    "game_options" => array(
         "turns" => 1000,
         "loadtime" => 3000,
         "turntime" => 1000
-	)
+    )
 );
 
 ?>
