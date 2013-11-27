6c2f8636432c232ac1846ddeb690f9aac7f2cff3
Francis Bellanger <bellanger.francis@gmail.com>
2013-11-27 16:03:44 +0100

    Up to 6 players !

--- server/ITWarsEngine/engine/run.bat
+++ server/ITWarsEngine/engine/run.bat
@@ -5,9 +5,10 @@ echo.
 
 REM TODO :
 REM    - specialiser le run pour chaque StarterKit
+REM    - Corriger l'ouverture du navigateur sur le replay
 
 
-REM Exemple d'appel : run map1.txt "node MyBot.js" "php MyBot.php"
+REM Example: run map1.txt "node MyBot.js" "php MyBot.php"
 
 
 REM Load config
@@ -18,15 +19,15 @@ for /F "tokens=1,* delims==" %%A in (run.conf) do (
 
 REM Check config
 if "x%map%" == "x" (
-	set error=Fichier de map non specifie
+	set error=Map file not specified
 	goto config_error
 )
 if not exist %map% (
-	set error=Fichier de map inexistant
+	set error=Map file not found
 	goto config_error
 )
 if "x%bot_cmd%" == "x" (
-	set error=Commande de lancement du bot non specifie
+	set error=Invalid command bot
 	goto config_error
 )
 REM Extract nb players from map
@@ -34,24 +35,24 @@ for /F "tokens=2 delims=\" %%a in ('echo %map%') do (
 	set nb_players=%%a
 )
 if "x%nb_players%" == "x" (
-	set error=Nombre de joueurs non specifie
+	set error=Player number not specified
 	goto config_error
 )
-echo %nb_players%| findstr /r "^[2-4]$">nul
+echo %nb_players%| findstr /r "^[2-6]$">nul
 if errorlevel 1 (
-	set error=Nombre de joueurs invalide
+	set error=Invalid player number
 	goto config_error
 )
 
-REM Define other bots
+REM Define other bots -- you may replace these commands with your own bots
 set bot1="java -cp engine.jar six.challenge.bot.BullyBot"
 set bot2="java -cp engine.jar six.challenge.bot.LooterBot"
 set bot3="java -cp engine.jar six.challenge.bot.RageBot"
 set bot4="java -cp engine.jar six.challenge.bot.RandomBot"
 
-echo Lancement de la partie a %nb_players% joueurs sur la carte %map%
+echo Starting game with %nb_players% players on map %map%
 echo.
-echo Dans le replay, le player 1 est toujours votre bot
+echo In the replay file, your bot is always player 1
 echo.
 
 :runEngine
@@ -59,32 +60,32 @@ set err_file=replay_err.txt
 if %nb_players% == 2 set run_cmd="%bot_cmd%" %bot1%
 if %nb_players% == 3 set run_cmd="%bot_cmd%" %bot1% %bot2%
 if %nb_players% == 4 set run_cmd="%bot_cmd%" %bot1% %bot2% %bot3%
+if %nb_players% == 5 set run_cmd="%bot_cmd%" %bot1% %bot2% %bot3% %bot4%
+if %nb_players% == 6 set run_cmd="%bot_cmd%" %bot1% %bot2% %bot3% %bot4% %bot2%
 
-REM Nettoyage partie precedente
+REM Cleaning previous game
 del %err_file% 2>NUL
 del replay_log.txt 2>NUL
 del visu\replay.js 2>NUL
 
 echo var replayJson=>visu\replay.js
 java -Duser.language=en -jar engine.jar %map% 1000 1000 replay_log.txt %run_cmd% 1>>visu\replay.js 2>%err_file%
-set winner=%errorlevel%
+if not "%errorlevel%" == "0" goto error
 
 REM Check if replay_err is empty
 for %%A in (%err_file%) do if not %%~zA==0 goto error
 
-echo.
-echo ====================================================================
-echo ====================================================================
-if %winner% == 1 echo                 VICTOIRE
-if not %winner% == 1 echo                 defaite
-echo ====================================================================
-echo ====================================================================
-echo.
-echo (appuyez sur une touche pour lancer la lecture du replay)
-pause
-
 REM Launch replay
-start visu/index.html
+REM FIXE start explorer visu/index.html
+echo.
+echo ====================================================================
+echo ====================================================================
+echo                    	Game over
+echo You may watch the replay by opening visu/index.html in your browser
+echo ====================================================================
+echo ====================================================================
+echo.
+pause
 goto end
 
 :error
@@ -104,12 +105,12 @@ pause
 goto end
 
 :config_error
-echo !!!!!!!!!!!!!!!!! Erreur !!!!!!!!!!!!!!!!!
+echo !!!!!!!!!!!!!!!!! Error !!!!!!!!!!!!!!!!!!
 echo.
-echo Config invalide :
+echo Invalid configuration :
 echo     %error%
 echo.
-echo Verifiez le fichier 'run.conf'
+echo Check the configuration file 'run.conf'
 echo.
 echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 echo.
