13c5482191de8cd0c7432871f73fe0b6dc78fb1f
gaelrenoux <gael.renoux@gmail.com>
2014-04-25 18:50:55 +0200

    Mise à jour pour nouvelles règles

--- starters/scala/MyBot.scala
+++ starters/scala/MyBot.scala
@@ -11,32 +11,39 @@ object MyBot extends App with Bot {
    * replace it with your own.
    * </p>
    */
-  override def doTurn(game: GameState): TraversableOnce[Order] = {
-
-    // (1) If we currently have a fleet in flight, just do nothing.
-    if (!game.myMilitaryFleets.isEmpty) {
-      return None
+  override def doTurn(game: GameState): Iterable[Order] = {
+    
+    /* if i have no more military planet, i can't do anything */
+    if (game.myMilitaryPlanets.isEmpty) {
+      return Nil
     }
 
-    // (2) Find my strongest military planet
-    val myMilitaryPlanets = game.myMilitaryPlanets
-    val source = if (myMilitaryPlanets.isEmpty) None else Some(myMilitaryPlanets.max(OrderingByStrength))
+    /* for each economic planet having more than 50 ships, send 50 ships to the nearest military planet */
+    val ecoOrders = game.myEconomicPlanets.filter { _.shipsNumber > 50 }.map { eco =>
+      val nearest = game.myMilitaryPlanets.min(new OrderingByDistanceTo(game, eco))
+      Order(eco, nearest, 50)
+    }
 
-    // (3) Find the weakest enemy or neutral planet
-    val notMyPlanets = game.notMyPlanets
-    val dest = if (notMyPlanets.isEmpty) None else Some(notMyPlanets.min(OrderingByStrength))
-
-    // (4) Send half the ships from my strongest planet to the weakest planet that I do not own.
-    if (source.isDefined && dest.isDefined) {
-      val ships = source.get.shipsNumber / 2
-      Some(Order(source.get, dest.get, ships))
-    } else {
+    val milOrder = if (game.myMilitaryFleets.size > 2 || game.myMilitaryPlanets.isEmpty || game.notMyPlanets.isEmpty) {
+      /* If more than two military fleets, or i have no more military planet, or there is no more victim */
       None
+    } else {
+      /* Else, send all ships from my strongest planet to its closest neighbour. */
+      val source = game.myMilitaryPlanets.max(OrderingByStrength)
+      val nearest = game.notMyPlanets.min(new OrderingByDistanceTo(game, source))
+      Some(Order(source, nearest, source.shipsNumber))
     }
+
+    ecoOrders ++ milOrder
   }
 
 }
 
 object OrderingByStrength extends Ordering[Planet] {
   override def compare(x: Planet, y: Planet) = x.shipsNumber - y.shipsNumber
+}
+
+class OrderingByDistanceTo(val state: GameState, val target: Planet) extends Ordering[Planet] {
+  override def compare(x: Planet, y: Planet) =
+    state.distance(target.id, x.id) - state.distance(target.id, y.id)
 }
