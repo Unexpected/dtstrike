bffdceb8ea6cce85afaac39626d78663b14463fd
Francis Bellanger <bellanger.francis@gmail.com>
2014-04-28 23:02:51 +0200

    Regles - ajout combats

--- website/application/views/game/game_rules.php
+++ website/application/views/game/game_rules.php
@@ -6,7 +6,6 @@
 	Vous commencez la partie en contrôlant une base militaire et 2 planètes de ravitaillement. Le jeu se déroule en tour par tour. 
 	<img src="<?php echo base_url('static/images/rules_start.png') ?>" />
 </p>
-<p>
 <div class="row rule-row">
 	<div class="span9">Les cercles et carrés colorés représentent les planètes de ravitaillement et bases militaires contrôlées par les joueur. Les carrés et cercles gris sont des planètes "neutres". Elles se défendent, mais ne produisent ni n'envoient aucune flotte militaire sur d'autres planètes. </div>
 </div>
@@ -18,7 +17,6 @@
 	<div class="span2"><img src="<?php echo base_url('static/images/rules_yellow_economic.png') ?>" class="pull-right rule-row-img" /></div>
 	<div class="span7">Deux planètes de ravitaillement appartenant au joueur jaune</div>
 </div>
-</p>
 <p>
 	Les planètes de ravitaillement que vous contrôlez vous fournissent des troupes fraîches capables uniquement de se défendre. Vous devez les envoyer sur vos bases militaires pour les transformer en soldats endurcis prêts à conquérir d'autres planètes. Une fois que vous avez amassés assez de vaisseaux, vous pouvez envoyer des flottes militaires depuis les bases militaires capturer les planètes neutres et ennemies. 
 </p>
@@ -26,7 +24,6 @@
 	A tout moment, si vous ne possédez plus de flottes militaires en vol ni aucune base militaire, vous êtes éliminé. Pour gagner, il vous suffit d'être le dernier joueur en vie dans la partie. 
 </p>
 <h3>Ravitaillement et flottes économiques</h3>
-<p>
 <div class="row rule-row">
 	<div class="span9">A chaque tour, les planètes économiques non neutres fournissent au joueur qui les contrôle autant de nouveaux vaisseaux que leur valeur de ravitaillement. </div>
 </div>
@@ -54,9 +51,7 @@
 	<div class="span2"><img src="<?php echo base_url('static/images/rules_yellow_eco_fleet_6.png') ?>" class="pull-right rule-row-img" /></div>
 	<div class="span7">Lorsque les vaisseaux arrivent sur la base militaire, la flotte disparait, le nombre de vaisseaux sur la base militaire augmente d'autant</div>
 </div>
-</p>
 <h3>Conquête et Flottes militaires</h3>
-<p>
 <div class="row rule-row">
 	<div class="span9">Vous pouvez à chaque tour envoyer des flottes militaires depuis vos bases militaires vers d'autres planètes. </div>
 </div>
@@ -80,5 +75,23 @@
 	<div class="span2"><img src="<?php echo base_url('static/images/rules_yellow_military_fleet_5.png') ?>" class="pull-right rule-row-img" /></div>
 	<div class="span7">Lorsqu'une autre flotte arrive sur la planète économique, elle appartient déjà au joueur jaune. Les vaisseaux sont également démantelés pour redevenir des troupes de défense uniquement</div>
 </div>
-</p>
 <h3>Règles de résolution des combats</h3>
+<p>Les règles suivantes s'appliquent pour les flottes militaires : </p>
+<ul>
+	<li>Seules les bases militaires permettent d'envoyer des flottes militaires</li>
+	<li>Lorsqu'une flotte militaire du joueur 1 avec F1 vaisseaux arrive sur une planète du joueur 1 contenant P1 vaisseaux, la flotte disparait et les vaisseaux de la flotte intègrent la planète de destination. On a donc F1 + P1 vaisseaux sur la planète appartenant au joueur 1. </li>
+	<li>Une flotte militaire du joueur 1 avec F1 vaisseaux arrive sur une planète du joueur 2 qui contient P2 vaisseaux. Si F1 <= P2, alors la flotte disparait et il reste (P2 - F1) vaisseaux sur la planète. Si F1 > P2, alors la planète passe sous le contrôle du joueur 1 avec (F1 - P2) vaisseaux</li>
+	<li>Si des flottes militaires appartenant aux joueurs 1, 2 et 3 avec respectivement F1, F2 et F3 vaisseaux arrivent au même tour sur une planète appartenant au joueur 3 (P3 vaisseaux). 
+		<ul>
+			<li>Si F1 > F2 et F1 > F3 + P3, alors la planète revient au joueur 1 qui se retrouve avec (F1 - max(F2, F3 + P3)) vaisseaux. </li>
+			<li>Si F3 + P3 > F1 et F3 + P3 > F2, alors la planète reste sous contrôle du joueur 3 avec (F3 + P3) - max(F1, F2) vaisseaux. </li>
+			<li>Si F1 == F2 et F1 > F3 + P3, la planète reste sous contrôle du joueur 3 avec 0 vaisseau. Il y a destruction mutuelle de l'ensemble des ennemis.</li>
+		</ul>
+	</li>
+</ul>
+<p>Les règles suivantes s'appliquent pour les flottes militaires : </p>
+<ul>
+	<li>Lorsqu'une flotte de ravitaillement du joueur 1 avec F1 vaisseaux arrive sur une planète du joueur 1, la flotte disparait et les vaisseaux de la flotte intègrent la planète de destination. On a donc F1 + P1 vaisseaux sur la planète appartenant au joueur 1. </li>
+	<li>Lorsqu'une flotte de ravitaillement du joueur 1 arrive sur une planète appartenant au joueur 2, la flotte est détruite. Les vaisseaux de ravitaillement sont perdus et n'infligent aucune perte au défenseur. La planète reste sous contrôle du joueur 2. </li>
+	<li>Lorsque des flottes de ravitaillement arrivent au même tour que des flottes militaires sur une planète, on résoud d'abord l'ensemble des batailles sans tenir compte des flottes de ravitaillement, puis, après avoir défini le nouveau propriétaire de la planète de destination, on fait arriver la flotte de ravitaillement selon les règles précédentes. </li>
+</ul>
