e34789d79e5157058660dd687b8222aeca7ac675
Sébastien SCHMITT <sebastien.schmitt@logica.com>
2014-04-29 15:50:31 +0200

    Modif placeholder

--- website/application/views/auth/register.php
+++ website/application/views/auth/register.php
@@ -8,7 +8,7 @@
 		echo form_fieldset('Complétez tous les champs');
 		form_hidden('action', 'register');
 		$this->bootstrap->input('E-mail :', array('name' => 'email', 'id' => 'email', 'class' => 'formfield', 'placeholder' => 'Addresse e-mail valide'), $email);
-		$this->bootstrap->input('Utilisateur :', array('name' => 'username', 'id' => 'username', 'class' => 'formfield', 'placeholder' => "> 6 caract [a-z, A-Z, 0-9, '-', '_' et '.']"), $username);
+		$this->bootstrap->input('Utilisateur :', array('name' => 'username', 'id' => 'username', 'class' => 'formfield', 'placeholder' => "> 3 caract [a-z, A-Z, 0-9, '-', '_' et '.']"), $username);
 		$this->bootstrap->password('Mot de passe :', array('name' => 'password', 'id' => 'password', 'class' => 'formfield', 'placeholder' => 'Mot de passe'), $password);
 		$this->bootstrap->password('Confirmation :', array('name' => 'password2', 'id' => 'password2', 'class' => 'formfield', 'placeholder' => 'Mot de passe'), $password2);
 		$this->bootstrap->select('Pays :', array('name' => 'country_code', 'id' => 'country_code', 'class' => 'formfield'), 'country_code', $countries, $country_code);
